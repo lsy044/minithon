2210fd9bcf8e2cdcc89395193d96eb23d4d8bebb
swimme <2sy100@likelion.org>
2018-08-03 20:40:52 +0900

    quiztest

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
 
   get '/random' => 'lottos#new' 
 
-  #get 'home/lotto'
-  #post 'home/result'
-  #get 'home/result'
-
-  root 'home#index'   
+    
   get '/faq' => 'home#faq'
+  get 'home/test'
+  get 'home/quiz'     # 임시페이지
+  get 'home/quizcard' # 임시페이지(블라인드공략테스트)
 
+  root 'home#index' 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin' # merge할 때 제일 마지막줄에 위치하게 해주세요!!
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
